020505a9ad5aaff24ed6d7b97f68eb6c4b243589
Bryan Franklin <44486392+fra99le@users.noreply.github.com>
2020-09-27 16:06:04 -0400

    Implemented initialization code.

--- maze.c
+++ maze.c
@@ -1,14 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "maze.h"
 
+int face_init(face_t *face, int *sizes, int d1, int d2) {
+    face->d1 = d1;
+    face->d2 = d2;
+    face->rows = sizes[d1];
+    face->cols = sizes[d2];
+    face->cells = calloc(face->rows*face->cols,sizeof(char));
+    memset(face->cells, 1, face->rows*face->cols);
+
+    return 0;
+}
+
+
 int maze_init(maze_t *maze, int numDimensions, int *sizes) {
+    maze->numDimensions = numDimensions;
+    maze->numFaces = (numDimensions*(numDimensions-1))/2;
+    maze->dimensions = calloc(numDimensions,sizeof(int));
+    memcpy(maze->dimensions,sizes,numDimensions*sizeof(int));
+    maze->faces = calloc(maze->numFaces,sizeof(face_t));
+    int face=0;
+    for(int d1 = 0; d1 < numDimensions; ++d1) {
+        for(int d2 = d1+1; d2 < numDimensions; ++d2) {
+            printf("face %i -> dimensions %i,%i\n", face, d1, d2);
+            face_init(&maze->faces[face++], sizes, d1, d2);
+        }
+    }
+
+    return 0;
 }
 
 
 int maze_generate(maze_t *maze) {
+    return 0;
 }
 
 
 int maze_export_stl(maze_t *maze, char *filename) {
+    return 0;
 }
-
-
--- maze.h
+++ maze.h
@@ -1,15 +1,23 @@
 #ifndef MAZE_H
 #define MAZE_H
 
+typedef struct maze_face {
+    int d1, d2;
+    int rows, cols;
+    char *cells;
+} face_t;
+
+int face_init(face_t *face, int *sizes, int d1, int d2);
+
 typedef struct maze {
     int numFaces;
+    int numDimensions;
     int *dimensions;
-    int ***cells;
+    face_t *faces;
 } maze_t;
 
 int maze_init(maze_t *maze, int numDimensions, int *sizes);
 int maze_generate(maze_t *maze);
 int maze_export_stl(maze_t *maze, char *filename);
-
 
 #endif // MAZE_H
