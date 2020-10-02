7a14f219bd4ad7e0434961061269eaedd019cd37
Bryan Franklin <44486392+fra99le@users.noreply.github.com>
2020-10-01 23:01:03 -0400

    Fixed some typoes.

--- pmg_config.h
+++ pmg_config.h
@@ -1,9 +1,9 @@
 /*
- * pmg_config.h
- * PuzzleMaze: A 3D puzzle model generator
+ * pmg_config.h.in
+ * PuzzleMaze: A 3D puzzle model genrator
  *
  * Copyright (c) 2020 Bryan Franklin. All rights reserved.
  */
-/* the configured options and settings for ndt */
+/* the configured options and settings for pmg */
 #define PMG_VERSION_MAJOR 0
 #define PMG_VERSION_MINOR 0
