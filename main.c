72949706579fb7a91d6d703b31718cca514f3f71
Bryan Franklin <44486392+fra99le@users.noreply.github.com>
2020-10-03 16:15:39 -0400

    Added call to maze_write().

--- main.c
+++ main.c
@@ -10,12 +10,11 @@
 int main(int argc, char **argv) {
     maze_t maze;
     int sizes[3] = {21, 21, 21};
+
     maze_init(&maze, 3, sizes);
-
     maze_generate(&maze);
-
+    maze_write(&maze, "output.txt");
     maze_export_stl(&maze, "output.stl");
-
     maze_free(&maze);
 
     return 0;
