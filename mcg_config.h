457f57d5c9bc0b7e02972888166d189cede2941a
Bryan Franklin <44486392+fra99le@users.noreply.github.com>
2020-11-22 15:00:55 -0500

    Fixed a missing PMG to MCG renaming.

--- mcg_config.h
+++ mcg_config.h
@@ -5,5 +5,6 @@
  * Copyright (c) 2020 Bryan Franklin. All rights reserved.
  */
 /* the configured options and settings for pmg */
-#define PMG_VERSION_MAJOR 0
-#define PMG_VERSION_MINOR 0
+#define MCG_VERSION_MAJOR 0
+#define MCG_VERSION_MINOR 0
+#define MCG_VERSION_PATCH 1
