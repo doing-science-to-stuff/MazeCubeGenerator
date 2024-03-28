da9910ecc6f685fb3e7d8d4144975bd49586e4b2
Bryan Franklin <44486392+fra99le@users.noreply.github.com>
2024-03-28 16:58:26 -0400

    Modified version numbers.

--- mcg_config.h
+++ mcg_config.h
@@ -5,6 +5,6 @@
  * Copyright (c) 2020-2023 Bryan Franklin. All rights reserved.
  */
 /* the configured options and settings for mcg */
-#define MCG_VERSION_MAJOR 1
-#define MCG_VERSION_MINOR 2
-#define MCG_VERSION_PATCH 0
+#define MCG_VERSION_MAJOR 0
+#define MCG_VERSION_MINOR 0
+#define MCG_VERSION_PATCH 1
